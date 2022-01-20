8b5ced34b2ad725160fde7af4cef83771ba45903
Roman <mazaxaka.tyt@gmail.com>
2022-01-20 12:53:52 +0200

    add counter for all carts

--- app.js
+++ app.js
@@ -6,7 +6,8 @@ const addToCartBtns = document.querySelectorAll(".add-cart");
 
 for(let i = 0; i < addToCartBtns.length; i++) {
     addToCartBtns[i].addEventListener ("click", function() {
-        productsCountEl.textContent = +productsCountEl.textContent + 1;
+        productsCountEl.textContent = +productsCountEl.textContent + +quantityInput[i].value;
+        quantityInput[i].value = 1;
     });
 }
 
@@ -76,45 +77,140 @@ $(document).ready(function(){
 
 
 // Счетчик товаров и ограничение по ним от 1 до 10
+
+
+// for (let i = 0; i < quantityInput.length; i++) {
+//     let currentValue = +quantityInput[i].value;
+//     toggleButtonState(currentValue, incrementBtns[i], decrementBtns[i]);
+// }
+
+// function toggleButtonState(count, incrementBtns, decrementBtns) {
+//   decrementBtns.disabled = count <= minCount;
+//   incrementBtns.disabled = count >= maxCount;
+// }
+
+// for(let i = 0; i < incrementBtns.length; i++) {
+//     incrementBtns[i].addEventListener("click", function () {
+//         let currentValue = +quantityInput[i].value;
+//         let nextValue = currentValue + 1;
+//         quantityInput[i].value = nextValue;
+        
+//         toggleButtonState(nextValue, incrementBtns[i], decrementBtns[i]);
+//         });
+// }
+
+// for(let i = 0; i < decrementBtns.length; i++) {
+//     decrementBtns[i].addEventListener("click", function () {
+//         let currentValue = +quantityInput[i].value;
+//         let nextValue = currentValue - 1;
+//         quantityInput[i].value = nextValue;
+      
+//         toggleButtonState(nextValue, incrementBtns[i], decrementBtns[i]);
+//       });
+// }
+
+ 
+// let audi = {
+//     model: "A4",
+//     color: "black",
+//     year: "2013",
+// };
+// let bmw = {
+//     model: "328",
+//     color: "grey",
+//     year: "2013",
+// };
+// let audi = {
+//     model: "A4",
+//     color: "black",
+//     year: "2013",
+// };
+
 let decrementBtns = document.querySelectorAll(".decrement-button");
 let incrementBtns = document.querySelectorAll(".increment-button");
 let quantityInput = document.querySelectorAll(".product-quantity input");
 
+function Counter(incrementBtn, decrementBtn, inputField, minCount = 1, maxCount = 5) {
+        this.domRefs = {
+            incrementBtn,
+            decrementBtn,
+            inputField,
+        };
+    
+
+    this.toggleButtonState = function() {
+        let count = this.domRefs.inputField.value;
+        this.domRefs.decrementBtn.disabled = count <= minCount;
+        this.domRefs.incrementBtn.disabled = count >= maxCount;
+    };
+
+    this.toggleButtonState();
+
+    this.increment = function () {
+        this.domRefs.inputField.value = +this.domRefs.inputField.value + 1;
+        this.toggleButtonState();
+      };
+
+    this.decrement = function () {
+        this.domRefs.inputField.value = +this.domRefs.inputField.value - 1;
+        this.toggleButtonState();
+      };
+
+    this.domRefs.incrementBtn.addEventListener("click", this.increment.bind(this));
+    
+    this.domRefs.decrementBtn.addEventListener("click", this.decrement.bind(this));
+}
+let counter1;
+for(i = 0; i < quantityInput.length; i++) {
+    counter1 = new Counter(incrementBtns[i],decrementBtns[i],quantityInput[i]);
+}
+
+
+   
+
+
+
+
+
+
+
+
+
 
 // для всех счетчиков
-decrementBtns.forEach((item) => {
-    item.addEventListener("click", function(){
-        let thisInput = this.parentElement.querySelector(".input");
-        let thisDecrementBtn = this.parentElement.querySelector(".decrement-button");
-        let thisIncrementBtn = this.parentElement.querySelector(".increment-button");
-        let currentValue = +thisInput.value;
-        let nextValue = currentValue - 1;
-        thisInput.value = nextValue;
-        if(nextValue < 10) {
-            thisIncrementBtn.disabled = false;
-        };
-        if(nextValue <= 1) {
-            thisDecrementBtn.disabled = true;
-        }
-    });
-});
+// decrementBtns.forEach((item) => {
+//     item.addEventListener("click", function(){
+//         let thisInput = this.parentElement.querySelector(".input");
+//         let thisDecrementBtn = this.parentElement.querySelector(".decrement-button");
+//         let thisIncrementBtn = this.parentElement.querySelector(".increment-button");
+//         let currentValue = +thisInput.value;
+//         let nextValue = currentValue - 1;
+//         thisInput.value = nextValue;
+//         if(nextValue < 10) {
+//             thisIncrementBtn.disabled = false;
+//         };
+//         if(nextValue <= 1) {
+//             thisDecrementBtn.disabled = true;
+//         }
+//     });
+// });
 
-incrementBtns.forEach((item) => {
-    item.addEventListener("click", function(){
-        let thisInput = this.parentElement.querySelector(".input");
-        let thisDecrementBtn = this.parentElement.querySelector(".decrement-button");
-        let thisIncrementBtn = this.parentElement.querySelector(".increment-button");
-        let currentValue = +thisInput.value;
-        let nextValue = currentValue + 1;
-        thisInput.value = nextValue;
-        if(thisInput.value > 1) {
-            thisDecrementBtn.disabled = false;
-        };
-        if(nextValue >= 10) {
-            thisIncrementBtn.disabled = true;
-        }
-    });
-})
+// incrementBtns.forEach((item) => {
+//     item.addEventListener("click", function(){
+//         let thisInput = this.parentElement.querySelector(".input");
+//         let thisDecrementBtn = this.parentElement.querySelector(".decrement-button");
+//         let thisIncrementBtn = this.parentElement.querySelector(".increment-button");
+//         let currentValue = +thisInput.value;
+//         let nextValue = currentValue + 1;
+//         thisInput.value = nextValue;
+//         if(thisInput.value > 1) {
+//             thisDecrementBtn.disabled = false;
+//         };
+//         if(nextValue >= 10) {
+//             thisIncrementBtn.disabled = true;
+//         }
+//     });
+// })
 
 
 
